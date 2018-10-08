21600d908b2c61b88f10e6620e1eeb4e192d5f95
Hussain Haider <hussainhaider490@gmail.com>
2018-10-08 23:52:42 +0500

    Update main.js

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -33,19 +33,51 @@ function progress_timer() {
     setTimeout(showTripsDiv, 3000);
 }
 
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
+function onSignInFailure() {
+    // Handle sign-in errors
+}
 
-//function initMap() {
-//    var location = {
-//        lat: 33.7294,
-//        lng: 73.0931
-//    };
-//    var map = new google.maps.Map(document.getElementById('map'), {
-//        zoom: 3,
-//        center: location
-//    });
-//    var marker = new google.maps.Marker({
-//            position: location,
-//            map: map
-//        
-//    });
-//}
+function facebooklogin() {
+    FB.login(function (response) {
+        if (response.authResponse) {
+            console.log('Welcome!  Fetching your information.... ');
+            FB.api('/me?fields=id,email,first_name,last_name', function (response) {
+                console.log('Good to see you, ' + JSON.stringify(response) + '.');
+            });
+        } else {
+            console.log('User cancelled login or did not fully authorize.');
+        }
+    }, {scope: 'public_profile,email'});
+}
+
+function facebooklogOut() {
+    FB.logout(function (response) {
+        console.log('Person is now logged out');
+    });
+}
+
+window.fbAsyncInit = function() {
+    FB.init({
+        appId      : '308338449961614',
+        cookie     : true,
+        xfbml      : true,
+        version    : 'v1.0'
+    });
+
+    FB.AppEvents.logPageView();
+};
+
+(function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
