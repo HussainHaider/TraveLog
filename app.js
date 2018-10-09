c66bb0d01fc717733c6c8c9473f33635b0f32894
Hussain Haider <hussainhaider490@gmail.com>
2018-10-09 12:42:40 +0500

    update Handlebars library and userProfile.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var sassMiddleware = require('node-sass-middleware');
+var hbs = require('express-handlebars');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/userProfile');
@@ -12,6 +13,8 @@ var exploreRouter = require('./routes/explore');
 var app = express();
 
 // view engine setup
+
+//app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts/'}));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
--- routes/userProfile.js
+++ routes/userProfile.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.render('Profile', { title: 'Profile | TraveLog',logo:'images/logo.jpg' });
+    res.render('Profile', { title: 'Profile | TraveLog',logo:'images/logo.jpg', anyArray: [1,2,3] });
 });
 
 router.get('/settings', function(req, res, next) {
