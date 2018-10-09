ff1d37d0d1d2a28d9a6160d42ab28c00ebad5bd9
Hussain Haider <hussainhaider490@gmail.com>
2018-10-09 10:49:05 +0500

    add explore & settings hbs files/js files

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var sassMiddleware = require('node-sass-middleware');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/userProfile');
+var exploreRouter = require('./routes/explore');
 
 var app = express();
 
@@ -28,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/profile', usersRouter);
+app.use('/explore', exploreRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/userProfile.js
+++ routes/userProfile.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
     res.render('Profile', { title: 'Profile | TraveLog' });
 });
 
+router.get('/settings', function(req, res, next) {
+    res.render('Settings', { title: 'Settings | TraveLog' });
+});
+
 module.exports = router;
