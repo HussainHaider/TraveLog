06c7f522fbc22c11a760ebfb44e66595778dc34f
Hussain Haider <hussainhaider490@gmail.com>
2018-10-10 21:34:47 +0500

    change facebook_scope_1

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ passport.use(new FacebookStrategy({
     }
 ));
 
-app.get('/auth/facebook', passport.authenticate('facebook', { scope: ['id','email', 'first_name','last_name'] }));
+app.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email', 'first_name','last_name'] }));
 
 app.get('/auth/facebook/callback',
     passport.authenticate('facebook', { successRedirect: '/profile',
