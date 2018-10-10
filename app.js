9d65642d080393dab6c3a0786caae36be2aabd16
Hussain Haider <hussainhaider490@gmail.com>
2018-10-10 22:05:36 +0500

    remove passport_session middleware

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 app.use(passport.initialize());
-app.use(passport.session());
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
