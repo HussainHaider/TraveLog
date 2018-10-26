a10d39ebd00bd62040355f5e8d19286ab923bb4b
Hussain Haider <hussainhaider490@gmail.com>
2018-10-26 08:04:57 +0500

    undefined the check

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ router.get('/', function(req, res, next) {
     console.log("index route: " + JSON.stringify(req.session));
     console.log("user: " + JSON.stringify(req.session.user));
     console.log("passport: " + JSON.stringify(req.session.passport));
-    if(req.session.user===null && req.session.passport===null){
+    if(req.session.user===undefined && req.session.passport===undefined){
         console.log("U have to sign In");
         res.render('index', { title: 'Login/Signup | TraveLog',logo:'images/logo.jpg',loginError: false,SignUpError: false });
     } else {
