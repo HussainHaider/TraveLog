cfe4b37e12a556c6e8c23cd1411672f746e60d64
Hussain Haider <hussainhaider490@gmail.com>
2018-10-26 07:41:35 +0500

    Update index.js route

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,13 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
     console.log("index route");
-  res.render('index', { title: 'Login/Signup | TraveLog',logo:'images/logo.jpg',loginError: false,SignUpError: false });
+    if(req.session.user===null && req.session.passport===null){
+        console.log("User already exit");
+        res.redirect('/profile');
+    } else {
+        console.log("U have to sign In");
+        res.render('index', { title: 'Login/Signup | TraveLog',logo:'images/logo.jpg',loginError: false,SignUpError: false });
+    }
 });
 
 module.exports = router;
